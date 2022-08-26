420c03d78dddadd84abfb08acae03a641f4be588
sherefkamelmurad <74113081+sherefkamelmurad@users.noreply.github.com>
2022-08-26 13:13:48 +0200

    Update calculator.js

--- javabasicsexam/calculator.js
+++ javabasicsexam/calculator.js
@@ -1,26 +1,33 @@
 function Calulator(num1, num2){
 
-    function sumby(){ 
+    function sum(){ 
         num1 = document.getElementById("firstNumber").value; 
         num2 = document.getElementById("secondNumber").value;
         document.getElementById("result").innerHTML = num1 +num2; 
     } 
-    function subtractby(){ 
+    function sub(){ 
         num1 = document.getElementById("firstNumber").value; 
         num2 = document.getElementById("secondNumber").value;
         document.getElementById("result").innerHTML = num1 - num2; 
     }
-    function multiplyby()
+    function multiply()
 {
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
         document.getElementById("result").innerHTML = num1 * num2;
 }
     
-    function dividby(){ 
+    function divid{ 
         num1 = document.getElementById("firstNumber").value; 
         num2 = document.getElementById("secondNumber").value; 
         document.getElementById("result").innerHTML = num1 / num2; 
     }
+    return{add,sub,multiply,divid} 
     
 }
+const calc = calculator(2,3)
+console.log(calc.add())
+console.log(calc.sub())
+console.log(calc.multiply())
+console.log(calc.divid())
+
